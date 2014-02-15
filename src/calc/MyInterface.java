7f82b5ec8ed53b9578314b4d64ff6f414b24dfcc
Ian Murphy <iano200@gmail.com>
2014-02-15 11:14:44 +0000

    Start of custom classes

---
+++ src/calc/MyInterface.java
@@ -0,0 +1,8 @@
+package calc;
+
+/**
+ * @author Ian Murphy - 20057028
+ *         Date: 15/02/14
+ */
+public class MyInterface {
+}
---
+++ src/calc/MyStack.java
@@ -0,0 +1,16 @@
+package calc;
+
+import java.util.Iterator;
+
+/**
+ * @author Ian Murphy - 20057028
+ *         Date: 15/02/14
+ */
+public class MyStack<T> implements Iterable<T> {
+
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+}

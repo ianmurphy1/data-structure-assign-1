607a7d6d079162f391f1040d1aded47113b3fb0e
Ian Murphy <iano200@gmail.com>
2014-02-24 16:57:54 +0000

    FX Started

--- src/calc/MyInterface.java
+++ src/calc/MyInterface.java
@@ -1,6 +1,11 @@
 package calc;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
@@ -16,6 +21,10 @@ public class MyInterface extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-
+        Parent root = FXMLLoader.load(getClass().getResource("interface.fxml"));
+        stage.setScene(new Scene(root, 200, 200));
+        stage.show();
     }
+
+
 }
